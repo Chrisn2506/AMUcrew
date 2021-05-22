760939f8ac0f74e3e2a56bf1dd7b27ed3b58e2ae
Chrisn2506 <84627662+Chrisn2506@users.noreply.github.com>
2021-05-23 01:49:00 +0200

    Update Graph_plotter.py

--- Graph_plotter.py
+++ Graph_plotter.py
@@ -1,5 +1,5 @@
-import pandas as p
+import pandas as p #Importing the library i will use to plot the graphs
 
-data = p.read_csv('Value_file.csv', header=0, usecols=['Reading_nr', 'YValue', 'Temperature', 'Humidity%', 'Pressure'])
+data = p.read_csv('Value_file.csv', header=0, usecols=['Reading_nr', 'YValue', 'Temperature', 'Humidity%', 'Pressure']) #Creates a dataframe with all the values i want to make a graph out of.
 
-data.plot(x = 'Reading_nr', y = 'Pressure', kind = 'scatter')
+data.plot(x = 'Reading_nr', y = 'Pressure', kind = 'scatter') #Plots the graph. Changed the y axis to get the different graphs. Scatter graph seemed like the best way to visualize the data.
